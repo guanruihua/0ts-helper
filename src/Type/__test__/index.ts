41e957d4f7ea834d75be7f23d3024bca6570e227
grh-gitee <3494242908@qq.com>
2022-02-11 10:30:53 +0800

    save code

--- src/Type/__test__/index.ts
+++ src/Type/__test__/index.ts
@@ -1,2 +1,102 @@
-import {} from 'rh-js-methods'
+import { rUtil, rFunction } from 'rh-js-methods'
 import * as Type from '../index'
+
+const onceLogGroup = rFunction.once(rUtil.logGroup)
+
+interface Example {
+  name: string
+  height: number
+  home: string
+}
+
+interface Angus {
+  name: string
+  height: number
+  wight: number
+}
+
+interface Person {
+  name: string
+  sex: string
+}
+{
+  type iaa = (name: string, age: number) => void
+  type newType = Type.Parameters<iaa>
+	let a: newType = ['ruihuag', 123]
+	let a1: newType[0] = 'ruihuag'
+	let a2: newType[1] = 123
+	onceLogGroup('Parameters',a, a1, a2)
+}
+
+{
+  type T0 = Type.ReturnType<() => string> // string
+  type T1 = Type.ReturnType<(s: string) => void> // void
+  type T2 = Type.ReturnType<() => {}> // {}
+  type T3 = Type.ReturnType<() => number[]> // number[]
+  type T4 = Type.ReturnType // any
+  type T5 = Type.ReturnType // any
+  type T6 = Type.ReturnType // Error
+  type T7 = Type.ReturnType // Error
+  let a: T0 = 'str'
+  onceLogGroup('ReturnType', a)
+}
+
+{
+  type newType = Type.Readonly<Person>
+  let a: newType = {
+    name: 'ruihuag',
+    sex: 'man'
+  }
+  // a.sex = 'woman'// 会出问题
+  onceLogGroup('Readonly', a)
+}
+
+{
+  type a = string | number
+  type newPerson = Type.Exclude<a, string>
+  let aa: newPerson = 123
+  onceLogGroup('iExclude', aa)
+}
+
+{
+  type newType = Type.Partial<Person>
+  let a: newType = {}
+  let b: newType = { name: 'ruihuag' }
+  let c: newType = { sex: 'man' }
+  let d: newType = { name: 'ruihuag', sex: 'man' }
+  onceLogGroup('Partail', a, b, c, d)
+}
+
+{
+  type newType = Type.Omit<Person, 'name'>
+  let a: newType = {
+    sex: 'man'
+  }
+  onceLogGroup('Omit', a)
+}
+
+{
+  type newType = Type.FilterType<Example, string>
+  let a: newType = {
+    name: 'ruihuag',
+    home: 'home'
+    // home: 'home'
+  }
+  onceLogGroup('FilterType', a)
+}
+
+{
+  type newAngus = Type.Pick<Angus, 'name' | 'height'> //{name:string;height:number}
+  let a: newAngus = {
+    height: 123,
+    name: 'ruihuag'
+  }
+  onceLogGroup('Pick', a)
+}
+
+{
+  type newType = Type.Clothes<Example, string> // type newType = "name | home"
+  let a: newType = 'home'
+  let b: newType = 'name'
+  onceLogGroup('Clothes', a, b)
+}
--- src/Type/index.ts
+++ src/Type/index.ts
@@ -1,3 +1,44 @@
-export interface iAnyObject {
-	[key: string]: any;
+// type 类型 工具
+
+// 获取types类型的key合集
+export type Clothes<source, types> = {
+  [K in keyof source]: source[K] extends types ? K : never
+}[keyof source]
+
+// 挑选指定类型成为新的类型
+export type Pick<T, K extends keyof T> = {
+  [P in K]: T[P]
 }
+
+// 过滤类型
+export type FilterType<Source, Types> = Pick<
+  Source,
+  {
+    [K in keyof Source]: Source[K] extends Types ? K : never
+  }[keyof Source]
+>
+
+/**
+ *  T : 待编辑的基本类型
+ *  K : 要删除的类型
+ */
+export type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
+
+// 将属性变成可选
+export type Partial<T> = {
+  [P in keyof T]?: T[P]
+}
+
+// 删除类型集合
+export type Exclude<T, U> = T extends U ? never : T
+
+// 所有属性变成只读
+export type Readonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
+// 获取函数 T 的返回类型
+export type ReturnType<T = any> = T extends (...args: any) => infer R ? R : any
+
+// 获取函数参数数组
+export type Parameters<T> = T extends (...arg: infer P) => void ? P : string
